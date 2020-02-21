42ea9d3c267916df4c00bf08b09aa8e86014bd25
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-02-21 16:17:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingField: Array(9).fill(null),
+      count: 0,
+      reset: ''
+    }
+
+  }
+
+  // static getDerivedStateFromProps(props, state) {
+  //   return {
+  //     playingField: props.appData.playingField
+  //   }
+  // }
+
+  clickHandler = (event) => {
+    let count = this.state.count;
+    let dataId = event.target.getAttribute('data-id');
+    let curentPlayingField = this.state.playingField;
+    if (event.target.innerText === '' || curentPlayingField[dataId] === null) {
+      if (count % 2 === 0) {
+        // event.target.innerText = 'X';  
+        curentPlayingField[dataId] = 'X';
+      } else {
+        // event.target.innerText = 'O';
+        curentPlayingField[dataId] = 'O';
+      }
+      this.setState({ count: count + 1 });
+      this.winner();
+    } else {
+      alert('field is occupied')
+    }
+  }
+
+  winner = () => {
+    let move = (this.state.count % 2 === 0) ? 'X' : 'O';
+    console.log(move)
+    if (this.state.count === 8) {
+      this.setState({ reset: 'reset' })
+    }
+  }
+  
+  resetApp = () => {
+    this.setState({ playingField: Array(9).fill(null) })
+    this.setState({ count: 0})
+  }
+
+  render() {
+    let appData = this.props.appData;
+    return (
+      <div className="tic-tac-toe">
+        <h1>{appData.title}</h1>
+        <p>{appData.description}</p>
+        <div className="playing-field">
+          {this.state.playingField.map((obect, i) => {
+            return (
+              <div key={'data-id' + i} data-id={i} className="playing-field__square" onClick={this.clickHandler}>{obect}</div>
+            )
+          })}
+        </div>
+        <div className="reset" onClick={this.resetApp}>{this.state.reset}</div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let appData = {
+title: "Tic-tac-toe",
+description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum iure ratione maiores, deleniti accusamus animi esse quos totam libero consequuntur, quod cumque veniam harum maxime, earum dicta et similique perspiciatis. Hic labore adipisci itaque recusandae praesentium assumenda eum non vitae temporibus? Quaerat odio deserunt praesentium assumenda molestiae doloremque aperiam et.",
+// playingField: Array(9).fill(null)
+}
+
+ReactDOM.render(<App  appData={appData}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
