7e73137733bf2d5e7811ec3039bd7b7cf76d2cca
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-07-31 16:08:23 +0300

    Updates

---
+++ precache-manifest.8da503fd5fd8634883e0d11c6df997b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d465a5f42a9a9748304bf64b3c3183f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8b5dbc88c6abd05f50c8",
+    "url": "/tic-tac-toe/static/css/main.d5a427fc.chunk.css"
+  },
+  {
+    "revision": "9246ad2390a790187bf7",
+    "url": "/tic-tac-toe/static/js/2.a8a95096.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.a8a95096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5dbc88c6abd05f50c8",
+    "url": "/tic-tac-toe/static/js/main.a9462c6e.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  }
+]);
